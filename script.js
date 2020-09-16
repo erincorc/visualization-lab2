dc801595d0251d7db40172cd890c71b6b9aeb668
Erin Corcoran <corcorec@bc.edu>
2020-09-16 19:04:05 -0400

    final version

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch("attractions.json")
   .then(response => response.json())
   .then(data => {
     attractions = data;
-    //   console.log("attractions", attractions);
+    console.log("attractions", attractions);
     attractions.sort(function(a, b) {
       let attracA = a.Visitors;
       let attracB = b.Visitors;
@@ -12,9 +12,9 @@ fetch("attractions.json")
       if (attracA > attracB) return -1;
       return 0;
     });
-    let short = attractions.slice(0,5);
-    //console.log(short);
-    renderBarChart(short);
+    let short = attractions.slice(0,5); 
+    console.log("short array", short);
+    renderBarChart(short); //rendering our initial 'all' chart when we first open web page
   });
 
 
@@ -35,6 +35,7 @@ function filterData(category) {
    *
    * **************************************************/
   
+  // need to fetch the data again because it is asynchronous and undefined if we don't fetch once more
   let attractions;
   fetch("attractions.json")
     .then(response => response.json())
@@ -51,7 +52,6 @@ function filterData(category) {
       if (category != "all") { // if we want all categories, don't want to filter b/c 'all' isn't a category  
       let a = attractions.filter(attraction => attraction.Category == category);
       let little = a.slice(0,5);
-
       renderBarChart(little);
       }
       else renderBarChart(attractions.slice(0,5));
@@ -62,7 +62,7 @@ function filterData(category) {
 // TODO: Define an event listener for the dropdown menu
 //       Call filterData with the selected category
 let cat;
-cat = document.querySelector("#attraction-category").addEventListener("change",function() {
+cat = document.querySelector("#attraction-category").addEventListener("change", function() {
   filterData(event.target.value);
 });
 
